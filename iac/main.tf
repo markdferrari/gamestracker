9829eb385da2f64f108edc291d0b1af9d01d6f90
Mark Ferrari <markdferrari@gmail.com>
2026-02-05 12:56:08 +0000

    feat: dns and cert
    Signed-off-by: Mark Ferrari <markdferrari@gmail.com>

---
+++ iac/main.tf
@@ -0,0 +1,39 @@
+terraform {
+  backend "s3" {
+    bucket = "632700996244-markdferrari-terraform-state"
+    key    = "wherecaniplayit/terraform.tfstate"
+    region = "eu-west-1"
+  }
+
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 6.0"
+    }
+  }
+}
+
+provider "aws" {
+  region = "eu-west-1"
+}
+
+# Route53 Hosted Zone
+resource "aws_route53_zone" "main" {
+  name = var.domain_name
+
+  tags = {
+    Name        = var.domain_name
+    Environment = "production"
+  }
+}
+
+# Output the nameservers
+output "nameservers" {
+  description = "Nameservers for whencaniplayit.com"
+  value       = aws_route53_zone.main.name_servers
+}
+
+output "zone_id" {
+  description = "Route53 Zone ID"
+  value       = aws_route53_zone.main.zone_id
+}
